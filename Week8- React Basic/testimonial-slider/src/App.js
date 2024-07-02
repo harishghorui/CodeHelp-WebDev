e46b92ff44419430cf8af0e03f3e4b50535ad9de
harishghorui <harishghorui2@gmail.com>
2024-07-02 22:28:31 +0530

    testimonial slider app created

---
+++ Week8- React Basic/testimonial-slider/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Testimonials from './components/Testimonials.js'
+
+function App() {
+  return (
+    <div className='w-full h-screen flex flex-col justify-center items-center'>
+
+      <div>
+        <h1>Our Testimonials</h1>
+      </div>
+
+      <div></div>
+
+      <Testimonials/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ Week8- React Basic/testimonial-slider/src/components/Testimonials.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from './Card'
+
+function Testimonials() {
+  return (
+    <div>
+        <Card/>
+    </div>
+  )
+}
+
+export default Testimonials
